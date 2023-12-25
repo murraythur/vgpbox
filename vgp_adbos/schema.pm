14c9ac5360247167dfe1eec91847bff5dc7c3e47
Arthur M <143713077+murraythur@users.noreply.github.com>
2023-12-25 20:18:22 -0300

    schema.pm

---
+++ vgp_adbos/schema.pm
@@ -0,0 +1,12 @@
+use utf8;
+package ADBOS::Schema;
+
+# Created by DBIx::Class::Schema::Loader
+#   não modifique esta parte do codigo!
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
